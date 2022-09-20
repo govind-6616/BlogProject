b8f632490ac705759fe3cf64a392279ee82f91db
govind <IT2024106@mlvti.ac.in>
2022-09-20 14:11:02 +0530

    update db

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
-// const DB='mongodb+srv://govind:govind@cluster0.heq3q.mongodb.net/mernstack?retryWrites=true&w=majority';
-const DB="mongodb://localhost:27017/blog";
+ const DB='mongodb+srv://govind:govind@cluster0.heq3q.mongodb.net/mernstack?retryWrites=true&w=majority';
+//const DB="mongodb://localhost:27017/blog";
 mongoose.connect(DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
